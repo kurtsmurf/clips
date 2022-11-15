fa124d299a00415740ee14b651c4d6397f8e6f94
Chris Smith <chrisronsmith@gmail.com>
2022-11-15 12:49:30 -0500

    fix AudioInput types

--- src/AudioInput.tsx
+++ src/AudioInput.tsx
@@ -1,23 +1,29 @@
 import { audioContext } from "./audioContext";
 
-export const AudioInput = (props) => (
+type Props = { onChange: (buffers: AudioBuffer[]) => void; };
+export const AudioInput = (props: Props) => (
   <input
     type="file"
     accept=".mp3, .wav, .m4a"
     multiple
-    onChange={(e) =>
-      Promise.all(
-        [...(e.target as HTMLInputElement).files].map(audioBufferOfFile),
-      ).then(props.onChange)}
+    onChange={(e) => {
+      const files = [...(e.currentTarget.files || [])];
+      Promise
+        .all(files.map(audioBufferOfFile))
+        .then(props.onChange);
+    }}
   />
 );
 
-const audioBufferOfFile = (file) =>
-  new Promise((resolve) => {
+const audioBufferOfFile = (file: File) =>
+  new Promise<AudioBuffer>((resolve, reject) => {
     const reader = new FileReader();
-    reader.onloadend = (e) =>
-      audioContext.decodeAudioData(e.target.result as ArrayBuffer).then(
-        resolve,
-      );
+    reader.onloadend = (e) => {
+      if (!(e.target?.result instanceof ArrayBuffer)) {
+        reject();
+        return;
+      }
+      audioContext.decodeAudioData(e.target.result, resolve);
+    };
     reader.readAsArrayBuffer(file);
   });
--- src/Tile.tsx
+++ src/Tile.tsx
@@ -1,4 +1,5 @@
-export const Tile = (props: { buffer: AudioBuffer }) => (
+type Props = { buffer: AudioBuffer }
+export const Tile = (props: Props) => (
   <figure>
     <p>{props.buffer.duration.toFixed(2)}s</p>
   </figure>
