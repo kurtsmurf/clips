f93ea45e54c5e605c8907261a193e85acbd1a16b
Chris Smith <chrisronsmith@gmail.com>
2022-11-16 11:45:19 -0500

    basic playability

--- src/Tile.tsx
+++ src/Tile.tsx
@@ -1,15 +1,33 @@
+import { audioContext } from "./audioContext";
 import { Clip } from "./Clip";
 
-type Props = { clip: Clip; }
+type Props = { clip: Clip };
 
-export const Tile = (props: Props) => (
-  <figure>
-    <svg viewBox="0 0 2 2">
-      <path d="m 0 1 h 2" stroke="black" stroke-width=".1" />
-    </svg>
-    <figcaption>
-      <p>{props.clip.name}</p>
-      <p>{props.clip.buffer.duration.toFixed(2)}s</p>
-    </figcaption>
-  </figure>
-);
+export const Tile = (props: Props) => {
+  let node: AudioBufferSourceNode | undefined;
+
+  const play = () => {
+    if (node) return;
+    node = audioContext.createBufferSource();
+    node.buffer = props.clip.buffer;
+    node.connect(audioContext.destination);
+    node.start();
+  };
+
+  const stop = () => {
+    node?.stop();
+    node = undefined;
+  };
+
+  return (
+    <figure>
+      <svg viewBox="0 0 2 2">
+        <path d="m 0 1 h 2" stroke="black" stroke-width=".1" />
+      </svg>
+      <figcaption onMouseDown={play} onMouseUp={stop}>
+        <p>{props.clip.name}</p>
+        <p>{props.clip.buffer.duration.toFixed(2)}s</p>
+      </figcaption>
+    </figure>
+  );
+};
