ba86395cf091acb8e7cfa5387904043b9a66b855
Chris Smith <chrisronsmith@gmail.com>
2022-11-17 15:10:32 -0500

    barely dancing waveforms :|

--- src/Tile.tsx
+++ src/Tile.tsx
@@ -2,49 +2,59 @@ import { audioContext, out } from "./audioContext";
 import { Clip } from "./Clip";
 import { createSignal } from "solid-js";
 
-
 type Props = { clip: Clip };
 
-const defaultPath = "m 0 1 h 2";
+const defaultPath = "m 0 0 h 2";
+
+const FFT_SIZE = 16;
 
 export const Tile = (props: Props) => {
   const [node, setNode] = createSignal<AudioBufferSourceNode | undefined>(
     undefined,
   );
-  const [d, setD] = createSignal(defaultPath)
+  const [d, setD] = createSignal(defaultPath);
 
   let animationFrame: number;
 
-  let x = 0;
-
   const stopAnimation = () => {
-    cancelAnimationFrame(animationFrame)
-    setD(defaultPath)
-    x = 0;
-  }
+    cancelAnimationFrame(animationFrame);
+    setD(defaultPath);
+  };
 
   const startAnimation = () => {
+    const analyser = audioContext.createAnalyser();
+    const samples = new Float32Array(FFT_SIZE);
+
+    node()?.connect(analyser);
+
+    const refreshSamples = () => {
+      analyser.getFloatTimeDomainData(samples);
+    };
+
     const tick = () => {
-      console.log("tick")
-      x += .01;
 
-      setD(`m ${x} 1 h 2`)
+      refreshSamples();
+      console.log(samples)
 
-      animationFrame = requestAnimationFrame(tick)
-    }
-    tick()
-  }
+      const blah = toPath(samples)
+
+      console.log(blah)
+
+      setD(blah);
+
+      animationFrame = requestAnimationFrame(tick);
+    };
+    tick();
+  };
 
   const play = () => {
     if (node()) return;
-
     const newNode = audioContext.createBufferSource();
     newNode.buffer = props.clip.buffer;
     newNode.connect(out);
     newNode.start();
     setNode(newNode);
-
-    startAnimation()
+    startAnimation();
   };
 
   const stop = () => {
@@ -52,7 +62,6 @@ export const Tile = (props: Props) => {
       node()?.stop();
     } catch {}
     setNode(undefined);
-
     stopAnimation();
   };
 
@@ -66,13 +75,37 @@ export const Tile = (props: Props) => {
       onTouchEnd={stop}
       onTouchCancel={stop}
     >
-      <svg viewBox="0 0 2 2">
-        <path d={d()} stroke="black" stroke-width=".1" />
+      <svg viewBox="0 -1 2 2">
+        <path d={d()} stroke="black" stroke-width=".03" fill="none"/>
       </svg>
       <figcaption>
         <p>{props.clip.name}</p>
         <p>{props.clip.buffer.duration.toFixed(2)}s</p>
       </figcaption>
     </figure>
+  );
+};
+
+const move = (
+  path: string,
+  { time, amplitude }: { time: number; amplitude: number },
+) => path + `M ${time}, ${amplitude} `;
+const lineTo = (
+  path: string,
+  { time, amplitude }: { time: number; amplitude: number },
+) => path + `L ${time}, ${amplitude} `;
+const normalize = (float: Float32Array[number]) => float;
+const toPath = (floats: Float32Array): string => {
+  const [first, ...rest] = floats.map(normalize);
+  const startingPoint = move("", { time: 0, amplitude: first });
+  return rest.reduce(
+    (path, float, index) => lineTo(
+      path,
+      {
+        time: (index + 1) / FFT_SIZE,
+        amplitude: float
+      }
+    ),
+    startingPoint,
   );
 };
