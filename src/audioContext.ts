a4f6dedb85cb790ba841745954c36db661da367d
Chris Smith <chrisronsmith@gmail.com>
2022-11-18 14:24:30 -0500

    fix title

--- src/audioContext.ts
+++ src/audioContext.ts
@@ -2,6 +2,7 @@ import { unmute } from "./unmute";
 
 export const audioContext = new AudioContext();
 
+// workaround for ios "silent mode" issue
 unmute(audioContext);
 
 export const out = audioContext.createDynamicsCompressor();
