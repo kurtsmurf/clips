f8adcb95d7ff0b77a2481f1c6fc70f4188aba4cc
Chris Smith <chrisronsmith@gmail.com>
2022-11-17 20:02:46 -0500

    accumulate clips

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createEffect, createSignal, For } from "solid-js";
+import { createSignal, For } from "solid-js";
 import { AudioInput } from "./AudioInput";
 import { Clip } from "./Clip";
 import { Tile } from "./Tile";
@@ -6,7 +6,7 @@ import { Tile } from "./Tile";
 export const App = () => {
   return (
     <>
-      <AudioInput onChange={setClips} />
+      <AudioInput onChange={onChange} />
       <div class="clips">
         <For each={clips()}>
           {(clip) => <Tile clip={clip} />}
@@ -18,4 +18,12 @@ export const App = () => {
 
 const [clips, setClips] = createSignal<Clip[]>([]);
 
-createEffect(() => console.log(clips()));
+const onChange = (newClips: Clip[]) => setClips(deduplicate(clips(), newClips));
+
+const deduplicate = (oldClips: Clip[], newClips: Clip[]) => {
+  const isNew = (clip: Clip) =>
+    !oldClips.find((oldClip) =>
+      oldClip.name === clip.name && oldClip.buffer.length === clip.buffer.length
+    );
+  return [...oldClips, ...newClips.filter(isNew)];
+};
