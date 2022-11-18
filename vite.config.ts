ed5fff7f90234aa99202947a15ad1f66908751ee
Kurt <chrisronsmith@gmail.com>
2022-11-18 09:58:15 -0500

    add deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import { qrcode } from 'vite-plugin-qrcode';
 
-
 export default defineConfig({
+  base: '/clips/',
   plugins: [
     solidPlugin(),
     qrcode(),
