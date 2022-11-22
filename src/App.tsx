3e587eede4901baaa8742288464a02bb7bcb7607
Chris Smith <chrisronsmith@gmail.com>
2022-11-21 21:59:07 -0500

    refactor App

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,20 @@ import { AudioInput } from "./AudioInput";
 import { Clip } from "./Clip";
 import { Tile } from "./Tile";
 
-export const App = () => {
-  return (
-    <>
-      <AudioInput onChange={addClips} />
-      <div class="clips">
-        <For each={clips()}>
-          {(clip) => <Tile clip={clip} />}
-        </For>
-      </div>
-    </>
-  );
-};
+export const App = () => (
+  <>
+    <AudioInput onChange={addClips} />
+    <Tiles />
+  </>
+);
+
+const Tiles = () => (
+  <div class="clips">
+    <For each={clips()}>
+      {(clip) => <Tile clip={clip} />}
+    </For>
+  </div>
+);
 
 const [clips, setClips] = createSignal<Clip[]>([]);
 
@@ -23,7 +25,8 @@ const addClips = (newClips: Clip[]) => setClips(deduplicate(clips(), newClips));
 const deduplicate = (oldClips: Clip[], newClips: Clip[]) => {
   const isNew = (clip: Clip) =>
     !oldClips.find((oldClip) =>
-      oldClip.name === clip.name && oldClip.buffer.length === clip.buffer.length
+      oldClip.name === clip.name &&
+      oldClip.buffer.length === clip.buffer.length
     );
   return [...oldClips, ...newClips.filter(isNew)];
 };
