3e2fecba2a36c0941c288330b2e27128121ab06b
Chris Smith <chrisronsmith@gmail.com>
2022-12-03 17:09:46 -0500

    renamed:    tests/example.spec.ts -> tests/index.spec.ts

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -6,9 +6,7 @@ test('test', async ({ page }, testInfo) => {
   })
 
   await page.goto('localhost:3000/clips/');
-  await page.getByRole('textbox').click();
-  // await page.getByLabel('Speed').click();
-  // await page.getByLabel('Gain').click();
+  await page.getByText('add clips').click();
   await page.getByLabel('Loop').check();
   await page.getByLabel('Hold').check();
   await page.getByText('sine.wav').click();
@@ -16,7 +14,7 @@ test('test', async ({ page }, testInfo) => {
   testInfo.attach(
     'screenshot',
     {
-      body: await page.locator("figure").screenshot(),
+      body: await page.locator("body").screenshot(),
       contentType: 'image/png'
     }
   );
