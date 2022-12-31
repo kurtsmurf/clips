8fdf2f7ad94d8f5549adaf0a1c6477a27a38c8b7
Chris Smith <chrisronsmith@gmail.com>
2022-12-31 12:59:09 -0500

    hide controls, change tile appearance

--- src/Tile.tsx
+++ src/Tile.tsx
@@ -79,31 +79,6 @@ export const Tile = (props: Props) => {
         <svg viewBox="0 -1 2 2">
           {/* waveform */}
           <path d={d()} stroke="black" stroke-width=".025" fill="none" />
-          {/* faux borders */}
-          <path
-            d={"M 0 -1 h 2"}
-            stroke="black"
-            stroke-width=".025"
-            fill="none"
-          />
-          <path
-            d={"M 0 1 h 2"}
-            stroke="black"
-            stroke-width=".025"
-            fill="none"
-          />
-          <path
-            d={"M 0 -1 v 2"}
-            stroke="black"
-            stroke-width=".025"
-            fill="none"
-          />
-          <path
-            d={"M 2 -1 v 2"}
-            stroke="black"
-            stroke-width=".025"
-            fill="none"
-          />
         </svg>
         <figcaption>
           <p>{props.clip.name}</p>
