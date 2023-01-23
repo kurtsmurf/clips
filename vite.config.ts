22575bf040bab84c2183fad28af35cfcb93fa6e7
Chris Smith <chrisronsmith@gmail.com>
2023-01-23 14:20:09 -0500

    can haz pwa?

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import { qrcode } from 'vite-plugin-qrcode';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: '/clips/',
   plugins: [
     solidPlugin(),
     qrcode(),
+    VitePWA(),
   ],
   server: {
     port: 3000,
